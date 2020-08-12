2531b81ef3434f72df479ca230d75b806f73ce38
laodingme <512740869@qq.com>
2020-08-12 16:52:53 +0800

    丁连杰8月12日的第二交提交

--- day2/file_op.py
+++ day2/file_op.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-#f = open("day2/yesterday2",'w', encoding="utf-8")  # 文件句柄
-f = open("day2/yesterday2",'a', encoding="utf-8")  # 文件句柄
-#a=append追加
-f.write("\nwhen i was young i listen to the radio\n")
-data = f.read()
-print('---read',data)
-f.close()
+#__author__="dlj"
+#data = open("day2/yesterday",encoding="utf-8").read()
+f = open("day2/yesterday", encoding="utf-8")#文件句柄
+data= f.read()
+data2 = f.read()
+print(data)
+#print(data)
