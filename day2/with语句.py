96719c561058cfc08c7691de2f2d20808ae5a495
dlj <512740869@qq.com>
2020-08-19 07:22:06 +0800

    add getdefaultencoding

--- day2/with语句.py
+++ day2/with语句.py
@@ -4,3 +4,4 @@ with open("day2/yesterday2", "r", encoding="utf-8") as f, \
         open("day2/yesterday2","r", encoding="utf-8") as f2:
     for line in f:
         print(line)
+print(sys.getdefaultencoding())
