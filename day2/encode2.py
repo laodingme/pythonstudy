5ca8bf842b271111ef9592e2a0eec67b3ee3ea07
dlj <512740869@qq.com>
2020-08-23 18:38:30 +0800

    函数初试

---
+++ day2/encode2.py
@@ -0,0 +1,8 @@
+#-*-coding:gbk-*-
+import sys
+print(sys.getdefaultencoding())
+__author__ = "dlj"
+s = "你哈"
+print(s.encode("gbk"))
+print(s.encode("utf-8"))
+print(s.encode("utf-8").decode("utf-8").encode("gb2312",))
